111033bc12ac0746633dccd26745045e7ef91da4
venica328 <veronika.satinova5@gmail.com>
2021-03-01 21:20:21 +0100

    Commit Domaca uloha 1 tyzden
    Doplnenie metod pre operacie s bytom

--- structures/memory_routines.cpp
+++ structures/memory_routines.cpp
@@ -1,52 +1,72 @@
 #include "memory_routines.h"
+#include <math.h>
 
 namespace structures {
 	byte & MemoryRoutines::byteSet(byte & B)
 	{
-		//TODO 01: MemoryRoutines
+		B = 255;
 		return B;
 	}
 	byte & MemoryRoutines::byteReset(byte & B)
 	{
-		//TODO 01: MemoryRoutines
+		B = 0;
 		return B;
 	}
 	byte & MemoryRoutines::byteXOR(byte & B)
 	{
 		//TODO 01: MemoryRoutines
+		byte result = 0;
+		while (B) {
+			result ^= B & 1;
+			B >>= 1;
+		}
 		return B;
 	}
 	byte & MemoryRoutines::byteSHL(byte & B)
 	{
-		//TODO 01: MemoryRoutines
+		B = B << 1;
 		return B;
 	}
 	byte & MemoryRoutines::byteSHR(byte & B)
 	{
-		//TODO 01: MemoryRoutines
+		B = B >> 1;
 		return B;
 	}
 	bool MemoryRoutines::byteNthBitGet(byte & B, int n)
 	{
-		//TODO 01: MemoryRoutines
-		return false;
+		byte a = B >> n;
+		if (a % 2 == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	byte & MemoryRoutines::byteNthBitTo0(byte & B, int n)
 	{
 		//TODO 01: MemoryRoutines
+		B ^= (0 ^ B) & (1UL << n);
 		return B;
 	}
 
 	byte & MemoryRoutines::byteNthBitTo1(byte & B, int n)
 	{
 		//TODO 01: MemoryRoutines
+		B ^= (-1 ^ B) & (1UL << n);
 		return B;
 	}
 
 	byte & MemoryRoutines::byteNthBitToggle(byte & B, int n)
 	{
 		//TODO 01: MemoryRoutines
+		if((B >> n) & 1) { 
+			byteNthBitTo0(B,n);
+		}
+		else
+		{
+			byteNthBitTo1(B, n);
+		}
 		return B;
 	}
 
